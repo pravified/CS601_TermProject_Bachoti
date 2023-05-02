125d24d6dcd02843a875e667666ed671d09f99a9
praveen bachoti <bachotip@gmail.com>
2023-05-02 00:50:09 -0400

    Updated Traveling to Music

--- javascript/music.js
+++ javascript/music.js
@@ -15,28 +15,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
   
   function displayInfluencer() {
     // Fetch of JSON influencers data
-    fetch("influencers.json")
+    fetch("bands.json")
       .then(response => response.json())
       .then(data => {
-        console.log("Fetched influencers data: ", data);
+        console.log("Fetched artists data: ", data);
   
         // Get DOM elements by ID
         let infNameSelect = document.getElementById("infNameDropdown");
         let infNameText = document.getElementById("infName");
         let infDescText = document.getElementById("infDesc");
-        let infPhoto = document.getElementById("influencerPhoto");
+        let bandPhoto = document.getElementById("bandPhoto");
         let infPhotoCaption = document.getElementById("infPhotoCaption");
 
         // For each to loop through array of data from JSON, checking to see if current array's name matches selected
         // If it does, write contents to DOM, and break
-        data.influencers.every((element) => {
+        data.bands.every((element) => {
           if(element.name === infNameSelect.value) {
             infNameText.innerHTML = element.name;
             infDescText.innerHTML = element.description;
             infPhotoCaption.innerHTML = element.name+".";
-            infPhoto.src = element.imgsrc;
+            bandPhoto.src = element.imgsrc;
+            bandPhoto.title = "Source: "+element.websrc;
+            bandPhoto.alt = "Source: "+element.websrc;
 
-            console.log("Influencer data displayed");
+            console.log("Bands data displayed");
             return false;
           }
           return true;
